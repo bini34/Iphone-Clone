6e057a6664dec81958715a2b9597c1df4513ecfd
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-02-15 11:58:15 +0300

    Refactor Navbar component to add responsive design and utility classes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
-
+import { appleImg, bagImg, searchImg } from "../utils"
+import { navLists } from "../constants"
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <header className="w-full sm:px-10 px-5 flex justify-between items-center py-5">
+      <nav className="flex w-full screen-max-width">
+        <img src={appleImg} alt="Apple" width={14} height={18} />
+
+        <div className="flex flex-1 justify-center max-sm:hidden">
+            {navLists.map((item, index) => (
+                <div key={index} className="px-5 text-sm text-gray-500 cursor-pointer hover:text-white transition-all">
+                {item}
+                </div>
+            ))}
+        </div>
+
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+           <img src={searchImg} alt="search" width={18} height={18}/> 
+           <img src={bagImg} alt="bag" width={18} height={18}/> 
+        </div>
+      </nav>
+    </header>
   )
 }
 
